68abbfb8b8a349b81b3910f1fee9b5d940e2f74a
yang lee <yanglee@macbookly.local>
2017-12-21 22:43:22 +0800

    Initial Commit

---
+++ hello-world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by yang lee on 2017/12/21.
+//  Copyright © 2017年 yang lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
